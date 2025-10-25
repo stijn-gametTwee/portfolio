1f8dbe8754201a9e69e22b4429c1b1564d6d7e1f
stijn_gametTwee <stijnpraet@icloud.com>
2025-10-25 17:31:38 +0200

    d

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,7 @@ import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://stijn-gamettwee.github.io/portfolio',
   vite: {
     plugins: [tailwindcss()],
   },
